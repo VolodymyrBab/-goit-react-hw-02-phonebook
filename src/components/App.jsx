05b5fb673f418c7724602b11c52961eb02606da7
VolodymyrBab <volbv@yahoo.com>
2023-06-17 17:04:06 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import { Component } from "react";
+import '../../src/index.css';
+// import { ContactList } from "./ContList/ContList";
+import { ContactForm } from "./ContForm/ContForm";
+
+
+
+export class App extends Component {
+
+state = {
+  contacts: [
+    {id: 'id-1', name: 'Jesus Christ', number: '777-77-77' },
+    {id: 'id-2', name: 'Steve Jobs', number: '444-99-22'},
+    {id: 'id-3', name: 'Stephen Wozniak', number: '645-17-79'},
+    {id: 'id-4', name: 'Hanna Chylka', number: '227-91-26'},],
+  filter: '',
+}
+
+
+render() {
+  const contacts = this.filteredContacts();
+  const filter = this.state.filter;
+  // console.log(contacts);
+  return <div className="container">
+    <div className="in_container">
+    <div>
+        <h2 className="title">Phonebook</h2>
+        {/* <Filter onFilterInput={this.onFilterInput} filter={filter} /> */}
+        <ContactForm onAddContact={this.onAddContact}/>
     </div>
-  );
+    <div>
+      <h2 className="title">Contacts</h2>
+        {/* <ContactList contacts={contacts} deleteContact={this.deleteContact} /> */}
+      </div>
+    </div>
+    <div className="circle"></div>
+  </div>
+}
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
 };
